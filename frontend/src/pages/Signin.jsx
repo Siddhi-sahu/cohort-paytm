2f17ae08db3d50fb807c5e00f92cf70b4280bbd1
Siddhi sahu <himanisahu739@gmail.com>
2024-08-04 15:23:00 +0530

    third commit

--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Heading from "../Components/Heading";
+import SubHeading from "../Components/SubHeading";
+import InputBox from "../Components/InputBox";
+import Button from "../Components/Button";
+import ButtonWarning from "../Components/ButtonWarning";
 
 const Signin = () => {
-  return <div></div>;
+  return (
+    <div className="bg-slate-300 h-screen flex justify-center">
+      <div className="flex flex-col justify-center">
+        <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
+          <Heading label={"Sign In"} />
+          <SubHeading label={"Enter your credentials to access your account"} />
+          <InputBox placeholder="abc@gmail.com" label={"Email"} />
+          <InputBox placeholder="123455" label={"Password"} />
+          <div className="pt-4">
+            <Button label={"Sign In "} />
+          </div>
+          <ButtonWarning
+            label={"Don't have an account?"}
+            buttonText={"Sign Up"}
+            to={"/signup"}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Signin;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,23 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import Heading from "../Components/Heading";
 import SubHeading from "../Components/SubHeading";
 import InputBox from "../Components/InputBox";
 import Button from "../Components/Button";
 import ButtonWarning from "../Components/ButtonWarning";
+import axios from "axios";
 
 const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <div className="bg-slate-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={"Sign up"} />
           <SubHeading label={"Enter your Information to create an account"} />
-          <InputBox placeholder={"John"} label={"First Name"} />
-          <InputBox placeholder={"Doe"} label={"last Name"} />
-          <InputBox placeholder={"abc@gmail.com"} label={"Email"} />
-          <InputBox placeholder={"12345"} label={"password"} />
+
+          <InputBox
+            onChange={(e) => {
+              setFirstName(e.target.value);
+            }}
+            placeholder={"John"}
+            label={"First Name"}
+          />
+
+          <InputBox
+            onChange={(e) => {
+              setLastName(e.target.value);
+            }}
+            placeholder={"Doe"}
+            label={"last Name"}
+          />
+
+          <InputBox
+            onChange={(e) => {
+              setUserName(e.target.value);
+            }}
+            placeholder={"abc@gmail.com"}
+            label={"Email"}
+          />
+
+          <InputBox
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder={"12345"}
+            label={"password"}
+          />
+
           <div className="pt-4">
-            <Button label={"Sign up"} />
+            <Button
+              onClick={() => {
+                axios.post("http://localhost:3000/api/v1/user/signup", {
+                  username,
+                  firstName,
+                  lastName,
+                  password,
+                });
+              }}
+              label={"Sign up"}
+            />
           </div>
           <ButtonWarning
             label={"Already have an account "}
